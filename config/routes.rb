2b563f336249fb46f70839c9295378f982d7f857
Sbstn Jmnz <sbstn.jmnz@yahoo.com>
2016-05-03 16:18:10 -0300

    update user done

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-
     namespace :api, path: '/webapp' do
       scope module: 'webapp' do
         resources :condos, only: [:index, :show] do
           resources :measure_processes, only: [:index, :show, :create] do
             match 'close', :on => :member, :via => [:post]
-
           end
           resources :charge_parameters, only: [:index] do
             collection do
@@ -14,14 +12,13 @@ Rails.application.routes.draw do
           end
           resources :sectors , only: [:show, :index]
           put 'sectors', :controller => 'sectors', :action => 'update'
-          resources :users, only: [:show, :create, :index]
+          resources :users, only: [:show, :create, :index, :update]
         end
         resources :plots, only: [:show]
         resources :meters, only: [:show]
         resources :sessions, only: [:create]
     end
   end
-
   namespace :api, path: '/mobileapp' do
     scope module: 'mobileapp' do
       resources :users, only: [:show] do
@@ -31,8 +28,6 @@ Rails.application.routes.draw do
       resources :sessions, only: [:create]
     end
   end
-
   root :controller => 'static', :action => '/public/index.html'
   get '*path' => 'application#index'
-
 end
