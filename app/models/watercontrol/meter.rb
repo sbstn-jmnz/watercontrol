c90f5f01d412dd9651b063b9b083ea135af11f0e
Sbstn Jmnz <sbstn.jmnz@yahoo.com>
2016-05-06 13:39:41 -0300

    refactor invoice model

--- app/models/watercontrol/meter.rb
+++ app/models/watercontrol/meter.rb
@@ -19,13 +19,61 @@ class Meter < ActiveRecord::Base
                                   users.name as last_measurer").last
   end
 
+  def current_measure_value
+    last_twelve_measures.last.value
+  end
+
+  def last_measure_value
+    last_twelve_measures[-2].value
+  end
+
+  def last_measure_process_id
+    last_twelve_measures[-2].measure_process_id
+  end
+
+  def normal_limit
+    300
+  end
+
+  def plot_number
+    plot.plot_number
+  end
+
   def idCorrentista
-    plot.owner.account_holder.idCorrentista #Penditene la conexion segura de correntistas con owner
+    plot.owner.account_holder.idCorrentista
   end
 
   def last_twelve_measures
     measures.last(12)
   end
+
+  def graph_data_array
+    # Returns a data array with lasts consumptions of the meter
+    data_array = []
+    last_twelve_measures.each_with_index do |measure, index|
+      break if i == measures_for_year.count -1
+      consumption = measures_for_year[i+1].value - measure.value
+      graph_data = {
+        consumption: consumption < 0 ? 0 : consumption,
+        created_at: measures_for_year[i+1].updated_at,
+        month_label: measures_for_year[i+1].updated_at.strftime("%b")
+      }
+      data_array.push(graph_data)
+    end
+    ensure_twelve_elements(data_array)
+  end
+
+  def ensure_twelve_elements(array)
+    if array.length < 12
+      pivot_date = array[0][:created_at]
+      (1..(12 - array.length)).each do |n|
+        array.unshift({
+          consumption: 0,
+          created_at: pivot_date - (n).month,
+          month_label: (pivot_date - (n).month).strftime("%b")
+          })
+      end
+   end
 
   Meter::STATUS.each do |status|
     define_method("#{status}?") do
