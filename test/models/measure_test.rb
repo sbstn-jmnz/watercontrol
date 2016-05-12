096c0e9aaf81344a16ac2128ad22785922a92230
Sbstn Jmnz <sbstn.jmnz@yahoo.com>
2016-05-12 13:42:50 -0300

    modif measure test

--- test/models/measure_test.rb
+++ test/models/measure_test.rb
@@ -4,6 +4,8 @@ class MeasureTest < ActiveSupport::TestCase
 
   setup do
     @meter =  create(:meter, number: 15, status: Meter::STATUS.first)
+    @process = create(:measure_process)
+    @measure = create(:measure_from_admin, meter_id: @meter_id, value: 500, status: Measure::STATUS.third)
     @user = create(:user, name:'Test Name', email: 'test@mail.com',password: 'secretpass', rut: '11.111.111-1')
   end
 
@@ -14,9 +16,18 @@ class MeasureTest < ActiveSupport::TestCase
   end
 
   test 'asscociations on meter and user' do
-    2.times { create(:measure_from_admin, value: 15, user_id: @user.id, meter_id: @meter.id) }
+    2.times { create(:measure_from_admin, value: 550, user_id: @user.id, meter_id: @meter.id) }
     assert_equal(2, @meter.measures.count, "Unexpected meter record count")
     assert_equal(2, @user.measures.count, "Unexpected user record count")
+  end
+
+  test 'it knows how to close a process' do
+    10.times { create(:measure_from_admin, value: 555, user_id: @user.id, status: Measure::STATUS.first,
+                      meter_id: @meter.id, measure_process_id: @process.id) }
+    Measure.close_process(@process.id)
+    Measure.where(measure_process_id: @process.id).each do |m|
+      assert_equal m.value,  Measure.where(status: Measure::STATUS.third, meter_id:@meter.id).last.value
+    end
   end
 
 end
